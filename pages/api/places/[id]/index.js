a08026a259f2a3612515c9f135158661de71f9da
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-21 17:41:23 +0200

    Querying the DB (.find, .findById)

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,23 +1,17 @@
-import { db_places } from "../../../../lib/db_places";
-import { db_comments } from "../../../../lib/db_comments";
+import dbConnect from "../../../../db/connect";
+import Place from "../../../../db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
   const { id } = request.query;
 
-  if (!id) {
-    return;
+  if (request.method === "GET") {
+    const place = await Place.findById(id).populate("comments");
+
+    if (!place) {
+      return response.status(404).json({ status: "Not Found" });
+    }
+
+    response.status(200).json(place);
   }
-
-  const place = db_places.find((place) => place._id.$oid === id);
-  const comment = place?.comments;
-  const allCommentIds = comment?.map((comment) => comment.$oid) || [];
-  const comments = db_comments.filter((comment) =>
-    allCommentIds.includes(comment._id.$oid)
-  );
-
-  if (!place) {
-    return response.status(404).json({ status: "Not found" });
-  }
-
-  response.status(200).json({ place: place, comments: comments });
 }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,5 +1,13 @@
-import { db_places } from "../../../lib/db_places";
+import dbConnect from "../../../db/connect";
+import Place from "../../../db/models/Place";
 
-export default function handler(request, response) {
-  return response.status(200).json(db_places);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+    return response.status(200).json(places);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
 }
