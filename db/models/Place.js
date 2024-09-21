d9e1474df6b6de789fafd091a98b9bcf458c9251
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-21 15:20:22 +0200

    add db/models/Place.js and db/models/Comments.js

---
+++ db/models/Place.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import Comment from "./Comments";
+
+const { Schema } = mongoose;
+
+const placeSchema = new Schema({
+  name: { type: String, required: true },
+  location: { type: String, required: true },
+  image: { type: String, required: true },
+  mapURL: { type: String, required: true },
+  description: { type: String, required: true },
+  comments: { type: [Schema.Types.ObjectId], ref: "Comment" },
+});
+
+const Place = mongoose.models.Place || mongoose.model("Place", placeSchema);
