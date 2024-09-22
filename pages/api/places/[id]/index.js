d4c0cb2f06b7a2c3face33698cb1ea4c17c6e216
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-22 16:27:16 +0200

    added PATCH and DELETE API Routes

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -7,11 +7,24 @@ export default async function handler(request, response) {
 
   if (request.method === "GET") {
     const place = await Place.findById(id).populate("comments");
-
     if (!place) {
       return response.status(404).json({ status: "Not Found" });
     }
-
     response.status(200).json(place);
+  }
+
+  if (request.method === "PATCH") {
+    const updatedPlace = request.body;
+    console.log("updatedPlace", updatedPlace);
+    const placeToUpdate = await Place.findByIdAndUpdate(id, updatedPlace);
+    response
+      .status(200)
+      .json(placeToUpdate, { status: "Place successfully updated." });
+  }
+
+  if (request.method === "DELETE") {
+    await Place.findByIdAndDelete(id);
+
+    response.status(200).json({ status: `Place ${id} successfully deleted.` });
   }
 }
