5110ba3aa25d9bba022faf848a71a795d801ad7e
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-23 14:18:47 +0200

    add DELETE API Route for comments

--- pages/api/places/[id]/comments.js
+++ pages/api/places/[id]/comments.js
@@ -4,7 +4,7 @@ import Comment from "../../../../db/models/Comments";
 
 export default async function handler(request, response) {
   await dbConnect();
-  const { id } = request.query;
+  const { id, commentID } = request.query;
 
   if (request.method === "POST") {
     try {
@@ -16,6 +16,23 @@ export default async function handler(request, response) {
       });
 
       response.status(201).json({ status: "Comment created" });
+    } catch (error) {
+      console.error(error);
+      response.status(400).json({ error: error.message });
+    }
+  }
+
+  if (request.method === "DELETE") {
+    try {
+      await Comment.findByIdAndDelete(commentId);
+
+      await Place.findByIdAndUpdate(id, {
+        $pull: { comments: commentId },
+      });
+
+      response
+        .status(200)
+        .json({ status: `Comment ${commentId} successfully deleted.` });
     } catch (error) {
       console.error(error);
       response.status(400).json({ error: error.message });
