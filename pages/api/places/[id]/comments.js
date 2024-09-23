0d13f0cadf0ff3a472dd64d89790f8ce58e09d64
Kristoffer Jans <jans.kiki@gmail.com>
2024-09-23 14:04:40 +0200

    deleted unnecessary code

--- pages/api/places/[id]/comments.js
+++ pages/api/places/[id]/comments.js
@@ -11,14 +11,6 @@ export default async function handler(request, response) {
       const commentData = request.body;
       const newComment = await Comment.create(commentData);
 
-      //   const place = await Place.findById(id);
-      //   if (!place) {
-      //     return response.status(404).json({ status: "Place not found" });
-      //   }
-
-      //   place.comments.push(newComment._id);
-      //   await place.save();
-
       await Place.findByIdAndUpdate(id, {
         $push: { comments: newComment._id },
       });
@@ -30,15 +22,3 @@ export default async function handler(request, response) {
     }
   }
 }
-
-// if (request.method === "POST") {
-//   try {
-//     const commentData = request.body;
-//     await Comment.create(commentData);
-
-//     response.status(201).json({ status: "Comment created" });
-//   } catch (error) {
-//     console.log(error);
-//     response.status(400).json({ error: error.message });
-//   }
-// }
